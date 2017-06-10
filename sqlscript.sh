c3f28b5bb8df59a05a2f98a310606058562f14c9
youni711 <youni711@gmail.com>
2017-06-09 17:14:37 -0700

    Delete sqlscript.sh

--- sqlscript.sh
+++
@@ -1,7 +0,0 @@
-
-
-show databases;
-
-create table mc-table(ID int auto_increment primary key, nameEntry varchar(100), ageEntry smallint, Date_Time datetime, comment varchar(255));
-
-show databases;
